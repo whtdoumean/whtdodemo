99756a1c99ea00e26cf5caae6ced4d28751e9cee
whtdoumean <whtdoumean0_0@outlook.com>
2023-06-14 21:51:39 +0300

    Little changes

---
+++ src/main/java/com/whtdotest/whtdodemo/entity/UserGroup.java
@@ -0,0 +1,24 @@
+package com.whtdotest.whtdodemo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_group")
+@Data // add getters and setters
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) // generation of uuid
+public class UserGroup {
+    @Id
+    @Column(name = "user_group_id")
+    @EqualsAndHashCode.Include
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid2")
+    private UUID id;
+    @Column
+    private String user_group_name;
+    // ???
+}
